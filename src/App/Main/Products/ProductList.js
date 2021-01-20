d1fbd8af69b2fbb5d03caa257b174eaaca094e64
taras_shorin <tshorin17@ukr.net>
2021-01-20 21:40:01 +0200

    Добавил 3 карточки и добавил PROPS

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -8,9 +8,49 @@ const ProductList = ()  =>  {
 
             <h2 className="page-title">Product List</h2>
                 <div className="row">
+
+
                      <div className="col-lg-6">
-                        <ProductListItem/>
+                        <ProductListItem
+                            name="Iphone X"
+                            description="This is Iphone X"
+                            type="phone"
+                            capacity="64"
+                            price="500"
+                        />
                     </div>
+
+                    <div className="col-lg-6">
+                        <ProductListItem
+                            name="Iphone 12"
+                            description="This is Iphone 12"
+                            type="phone"
+                            capacity="256"
+                            price="1500"
+                        />
+                    </div>
+
+                    <div className="col-lg-6">
+                        <ProductListItem
+                            name="Iphone XS"
+                            description="This is Iphone XS"
+                            type="phone"
+                            capacity="128"
+                            price="1000"
+                         />
+                    </div>
+
+                    <div className="col-lg-6">
+                        <ProductListItem
+                            name="Iphone X"
+                            description="This is Iphone 8 Plus"
+                            type="phone"
+                            capacity="64"
+                            price="500"
+                        />
+                    </div>
+
+
                     
                 </div>
          
--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import './ProductListItem.css'
 
-const ProdustListItem = () => {
+const ProdustListItem = (props) => {
+    console.log(props)
     return (
             <div className="product-list-item">
 
-                <div className="product-title">Iphone X</div>
-                <div className="product-description">This is Iphone X</div>
-                <div className="product-features">Type: phone</div>
-                <div className="product-features">Capacity: 64Gb</div>
-                <div className="product-price">$ 500</div>
+                <div className="product-title">{props.name}</div>
+                <div className="product-description">{props.description}</div>
+                <div className="product-features">Type: {props.type}</div>
+                <div className="product-features">Capacity: {props.capacity}Gb</div>
+                <div className="product-price">$ {props.price}</div>
                 <div className="btn-add-to-cart">Add to cart</div>
 
             </div>
