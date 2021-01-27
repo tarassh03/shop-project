cefe841f8f8fba5e66c18bc440104a6d2ed5c2b7
taras_shorin <tshorin17@ukr.net>
2021-01-27 15:25:23 +0200

    Убрали props и заменили его

--- src/App/Main/Products/ProductListItem.js
+++ src/App/Main/Products/ProductListItem.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './ProductListItem.css'
 
-const ProdustListItem = (props) => {
-    console.log(props)
+const ProdustListItem = ({
+                name,
+                description,
+                type,
+                capacity,
+                price,
+}) => {
+   
     return (
             <div className="product-list-item">
 
-                <div className="product-title">{props.name}</div>
-                <div className="product-description">{props.description}</div>
-                <div className="product-features">Type: {props.type}</div>
-                <div className="product-features">Capacity: {props.capacity}Gb</div>
-                <div className="product-price">$ {props.price}</div>
+                <div className="product-title">{name}</div>
+                <div className="product-description">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity}Gb</div>
+                <div className="product-price">$ {price}</div>
                 <div className="btn-add-to-cart">Add to cart</div>
 
             </div>
