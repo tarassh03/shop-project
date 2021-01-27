dcc5b2b4a40bf56164fe810a3794981fee59caf2
taras_shorin <tshorin17@ukr.net>
2021-01-27 16:06:22 +0200

    Добавили масив products

--- src/App/Main/Products/ProductList.js
+++ src/App/Main/Products/ProductList.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ProductListItem from './ProductListItem'
+import products from './products'
 
 const ProductList = ()  =>  {
 
@@ -9,49 +10,20 @@ const ProductList = ()  =>  {
             <h2 className="page-title">Product List</h2>
                 <div className="row">
 
+                    {
+                        products.map((product) => (
+                            <div className="col-lg-6">
+                            <ProductListItem
+                            name={product.name}
+                            description={product.description}
+                            type={product.type}
+                            capacity={product.capacity}
+                            price={product.price}
+                            />
+                        </div>
+                        ))
+                    }
 
-                     <div className="col-lg-6">
-                        <ProductListItem
-                            name="Iphone X"
-                            description="This is Iphone X"
-                            type="phone"
-                            capacity="64"
-                            price="500"
-                        />
-                    </div>
-
-                    <div className="col-lg-6">
-                        <ProductListItem
-                            name="Iphone 12"
-                            description="This is Iphone 12"
-                            type="phone"
-                            capacity="256"
-                            price="1500"
-                        />
-                    </div>
-
-                    <div className="col-lg-6">
-                        <ProductListItem
-                            name="Iphone XS"
-                            description="This is Iphone XS"
-                            type="phone"
-                            capacity="128"
-                            price="1000"
-                         />
-                    </div>
-
-                    <div className="col-lg-6">
-                        <ProductListItem
-                            name="Iphone X"
-                            description="This is Iphone 8 Plus"
-                            type="phone"
-                            capacity="64"
-                            price="500"
-                        />
-                    </div>
-
-
-                    
                 </div>
          
              </>
---
+++ src/App/Main/Products/products.js
@@ -0,0 +1,35 @@
+const products = [
+    {
+        name:"Iphone X",
+        description:"This is Iphone X",
+        type:"phone",
+        capacity:"64",
+        price:"500",
+    },
+    {
+        name:"Iphone XS",
+        description:"This is Iphone XS",
+        type:"phone",
+        capacity:"256",
+        price:"1500",
+    },
+    {
+        name:"Iphone 12",
+        description:"This is Iphone X",
+        type:"phone",
+        capacity:"64",
+        price:"2000",
+    },
+    {
+        name:"Iphone 8",
+        description:"This is Iphone 8",
+        type:"phone",
+        capacity:"64",
+        price:"1000",
+    },
+
+]
+
+
+
+export default products
