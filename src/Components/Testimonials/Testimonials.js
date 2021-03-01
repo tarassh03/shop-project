710b88a43a02f8c5b479347a32aed5cd9404717c
taras_shorin <tshorin17@ukr.net>
2021-03-01 17:59:26 +0200

    Добавили Testimonials

--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -2,25 +2,87 @@ import React,{useState} from 'react'
 
 const Testimonials = () => {
 
-    const [count,setCount] = useState(0);
-    const [color,setColor] =useState('green');
-
-    const toggleColor = () => {
-        setColor((value) => (
-            value === 'red' ? 'green' : 'red'
-        ))
-    }
+    const arrTestimonials = [
+        {name:"Jack",text:"Все очень нравится и товар и работа интернет- магазина. Уже пол года заказываю вещи. И за пол года полное доверие к магазину. Спасибо вам за вашу оперативную работу."},
+        {name:"Вікторія",text:"Тут все завжди швидки і якісно! Дякую"}
+    ]
 
 
+   const [testimonials,setTestimonials] = useState(arrTestimonials)
+   const [newTestimonials,setNewTestimonials] = useState({
+       name:"",
+       text:"",
+   })
+
+   const handleNameChange = (e) => (
+       setNewTestimonials((value) => ({
+           ...value,
+           name:e.target.value
+       }))  
+   )
+
+   const handleTextChange = (e) => (
+    setNewTestimonials((value) => ({
+        ...value,
+        text:e.target.value
+    }))  
+)
+
+    const onSend = (e) => {
+        e.preventDefault();
+        setNewTestimonials({
+            name:"",
+            text:"",
+        })
+        setTestimonials((value) => {
+            return [...value,newTestimonials]
+        })
+    }    
+   
     return (
         <>
-        <div>
-            <p>Color: {color}</p>
-            <button onClick={() => toggleColor()}>Change Color</button>
-        </div>
-            <button onClick={() => setCount(count-1)} >-</button>
-            <input value={count} type='text' readOnly />
-            <button onClick={() => setCount(count+1)}>+</button>
+            <h1 className="page-title">Testimonials</h1>
+            {
+                testimonials.map((item,i) => (
+                    <div 
+                        key={i}
+                        style={{
+                        backgroundColor:"rgba(255,255,255,0.75)",
+                        padding:"10px"
+                    }}>
+                        <div style={{marginBottom:"15px"}}>Name: {item.name}</div>
+                        <div style={{marginBottom:"15px"}}>Message: {item.text}</div>
+                        <hr/>
+                    </div>
+                ))
+            }
+
+            <form
+                 style={{marginTop:"15px"}}
+                 onSubmit={onSend}
+                 
+            >
+                <div>
+                    <input 
+                        placeholder="Your name"
+                        type="text"
+                        value={newTestimonials.name}
+                        onChange={handleNameChange}
+                    
+                    />
+                </div>
+                <div>
+                    <textarea
+                        placeholder="Your message"
+                        cols="30"
+                        rows="10"
+                        value={newTestimonials.text}
+                        onChange={handleTextChange}
+                    ></textarea>
+                </div>
+
+                <button>Leave a comment</button>
+            </form>
 
         </>
     )
